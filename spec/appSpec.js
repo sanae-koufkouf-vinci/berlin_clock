f00a15b66a30e39ac63db70aed2bbf1fa3d4de03
koufkouf <sanae.koufkouf@student.vinci.be>
2024-11-09 17:09:34 +0100

    add: manage the case 15 hours for line 1

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -549,5 +549,14 @@ describe("berlinClockLine1 function should return the 5-hours line of the Berlin
 
         expect(result).toBe("RR--");
     });
+
+    it("berlinClockLine1 should return RR-- when given time with hours = 11", function() {
+        const date = new Date();
+        date.setHours(11);
+
+        const result = main.berlinClockLine1(date);
+
+        expect(result).toBe("RR--");
+    });
 });
  
--- src/app.js
+++ src/app.js
@@ -115,7 +115,7 @@ export class Main {
 
    berlinClockLine1(time){
     if(time.getHours()>=5 && time.getHours()<10)return("R---");
-    if(time.getHours()===10)return("RR--");
+    if(time.getHours()>=10 && time.getHours()<15)return("RR--");
     return("----");
 
    }
