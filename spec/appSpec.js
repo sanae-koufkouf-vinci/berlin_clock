405c39cba4732c6f5e9f74a174378b616ffa24ee
S-Berdii <sami.berdii@student.vinci.be>
2024-11-09 16:46:54 +0100

    add: manage the case 7 hours for line 2

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -427,5 +427,14 @@ describe("berlinClockLine2 function should return the single hours line of the B
 
         expect(result).toBe("R---");
     });
+
+    it("berlinClockLine2 should return RR-- when given time with hours = 7", function() {
+        const date = new Date();
+        date.setHours(7);
+
+        const result = main.berlinClockLine2(date);
+
+        expect(result).toBe("RR--");
+    });
 });
  
--- src/app.js
+++ src/app.js
@@ -89,7 +89,7 @@ export class Main {
        // line 2
        berlinClockLine2(time){
               if(time.getHours()===1||time.getHours()===6)return("R---");
-              if(time.getHours()===2)return("RR--");
+              if(time.getHours()===2||time.getHours()===7)return("RR--");
               if(time.getHours()===3)return("RRR-"); 
               if(time.getHours()===4)return("RRRR");    
               return("----");
