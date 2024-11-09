d2e2ed7d89af494294bcbe3aa22eebb03ef4cb0f
koufkouf <sanae.koufkouf@student.vinci.be>
2024-11-09 14:38:52 +0100

    add: manage the case 1 minute

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -6,8 +6,16 @@ describe("berlinClock function should return the time in Berlin Clock ", functio
     it("berlinClock should return ---- when given time with minutes = 0 ", function() {
         const date = new Date();
         date.setMinutes(0);
-        const result = main.berlinClock(date.getTime());
+        const result = main.berlinClock(date);
         expect(result).toBe("----");
+    });
+
+
+    it("berlinClock should return ---J when given time with minutes = 1 ", function() {
+        const date = new Date();
+        date.setMinutes(1);
+        const result = main.berlinClock(date);
+        expect(result).toBe("---J");
     });
 });
  
