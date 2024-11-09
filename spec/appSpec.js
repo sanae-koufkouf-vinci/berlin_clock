f4d34764b14de440677729797071b35a5518d2df
barbara-morochuico-vinci <barbara.morochuico@student.vinci.be>
2024-11-09 14:52:04 +0100

    add: manage the case 4 minutes

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -32,6 +32,13 @@ describe("berlinClock function should return the time in Berlin Clock ", functio
         expect(result).toBe("JJJ-");
     });
 
+    it("berlinClock should return JJJJ when given time with minutes = 4 ", function() {
+        const date = new Date();
+        date.setMinutes(4);
+        const result = main.berlinClock(date);
+        expect(result).toBe("JJJJ");
+    });
+
     
 });
  
--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 export class Main {
 
        berlinClock(time){
+        if(time.getMinutes()===4)return("JJJJ");
         if(time.getMinutes()===3)return("JJJ-");
         if(time.getMinutes()===2)return("JJ--");
         if(time.getMinutes()===1)return("J---");
